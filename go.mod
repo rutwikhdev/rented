3eb5a00d2b44949229f082973fc92ba09885f204
Rutwik Hiwalkar <rutwikhdev@gmail.com>
2026-06-21 17:48:07 +0100

    chore(sec): use env file for storing db url

--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/knadh/koanf/maps v0.1.2 // indirect
+	github.com/knadh/koanf/providers/env v1.1.0 // indirect
 	github.com/labstack/gommon v0.5.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.22 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,13 @@
 package config
 
 import (
-	"github.com/knadh/koanf/parsers/toml"
+	"strings"
+
+	"github.com/knadh/koanf/providers/env"
 	"github.com/knadh/koanf/providers/file"
 	"github.com/knadh/koanf/v2"
+
+	"github.com/knadh/koanf/parsers/toml"
 )
 
 type Config struct {
@@ -28,6 +32,15 @@ func Load(path string) (*Config, error) {
 	k := koanf.New(".")
 
 	if err := k.Load(file.Provider(path), toml.Parser()); err != nil {
+		return nil, err
+	}
+
+	if err := k.Load(env.Provider("RENTED_", ".", func(s string) string {
+		return strings.Replace(
+			strings.ToLower(strings.TrimPrefix(s, "RENTED_")),
+			"_", ".", -1,
+		)
+	}), nil); err != nil {
 		return nil, err
 	}
 
